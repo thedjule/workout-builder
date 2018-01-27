551e5be780dd8dc5cd57e7ad31e4563bb05d9487
Srdjan Tomovic <srdjan.tomovic@gmail.com>
2018-01-28 00:46:21 +0100

    Fixed a bug in Header Methods, PUT and PATCH were not allowed in preflight response

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -17,7 +17,7 @@ class Cors
     {
         return $next($request)
             ->header('Access-Control-Allow-Origin', '*')
-            ->header('Access-Control-Allow-Methods', 'GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS')
+            ->header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, PATCH, DELETE')
             ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     }
 }
